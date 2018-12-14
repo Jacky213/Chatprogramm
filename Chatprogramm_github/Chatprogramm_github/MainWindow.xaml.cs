d401cf91fe9c79fc0003903d662cef26f07e797f
Jacky213 <jkaefer@bluewin.ch>
2018-12-14 12:09:09 +0100

    Signed-off-by: Jacky213 <jkaefer@bluewin.ch>

--- Chatprogramm_github/Chatprogramm_github/MainWindow.xaml.cs
+++ Chatprogramm_github/Chatprogramm_github/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* 14.12.2018 von Luca Katzenberger und Jacqueline Kaefer
+ * 
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,18 +16,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Chatprogramm_github
-{
-    //HI
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
+{    
+   
     public partial class MainWindow : Window
-    {
+    {   //Globale Variablen
+
+        const int port = 54546;
+        const string broadcastadress = "255.255.255.255";
+        UdpClient nachrichtenempfänger = new UdpClient(broadcastadress, port);
+        UdpClient nachrichtensender = new UdpClient(port);
+        Thread empfängerThread;
+
         public MainWindow()
         {
             InitializeComponent();
+            nachrichtensender.EnableBroadcast = true;
+
+            ThreadStart start = new ThreadStart(Receiver);
+
+
+        }
+        
+        public void Receiver()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port); //ANy überwacht alle Ipadressend es Netzwerks
+            
+        }
+        public void InitializeSender()
+        {
+            nachrichtenempfänger = new 
         }
     }
 }
