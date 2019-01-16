3935a6095ae9d59f2bf3e1a46f5d792c46b26b0a
Luca456 <luca-katzenberger@t-online.de>
2019-01-16 15:21:46 +0100

    Username_laden Methode Erstellung der Sicherungsdatei und abspeichern von neuen Nachrichten
    Signed-off-by: Luca456 <luca-katzenberger@t-online.de>

--- Chatprogramm_github/Speicherung.cs
+++ Chatprogramm_github/Speicherung.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Xml;
-using System.Xml.Serialization;
 using System.IO;
 
 
@@ -13,45 +8,119 @@ namespace Chatprogramm_github
 
     class Speicherung
     {
+        //Pfad unter dem die SIcherungsdatei gespeichert wird
         const string pfad = @"Sicherungsdatei.xml";
 
-        public static void Speichern(User mainuser, User aktueller_Chatpartner, Nachricht empfangeneNachricht)
+        public static void Speichern(User Mainuser, Nachricht empfangeneNachricht)
         {
-            //Entscheidung: 
+            //Sicherungsdatei laden
             XmlDocument Sicherungsdatei = new XmlDocument();
-            if (!File.Exists(pfad))
+            Sicherungsdatei.Load(pfad);
+            
+            //Unter welchem Kontakt soll die Nachricht gespeichert werden
+            User Chatpartner = new User();
+            if (empfangeneNachricht.Sender == Mainuser)
             {
-                XmlNode Mainusernode = Create_Usernode("mainuser", mainuser.Username);
-                Sicherungsdatei.AppendChild(Mainusernode);
-
-                XmlNode Chats = Sicherungsdatei.CreateElement("Chats");
-                Sicherungsdatei.AppendChild(Chats);
-
-                XmlNode Chatkontakt = Create_Usernode("Chatkontakt", aktueller_Chatpartner.Username);
-                Sicherungsdatei.AppendChild(Chatkontakt);
-
-
-                XmlNode Create_Usernode(string nodename, string username)
-                {
-                    XmlNode user_Node = Sicherungsdatei.CreateElement(nodename);
-                    XmlAttribute Username = Sicherungsdatei.CreateAttribute("username");
-                    Username.Value = username;
-                    return user_Node;
-                }
+                Chatpartner = empfangeneNachricht.Empfänger;
             }
             else
             {
-                //Load, Node suchen mit aktuellem Kontakt; neue Nachricht speichern unter aktuellem Kontakt
-                //Diese Funktion muss von der Empfangenmethode aufgerufen werden
+                Chatpartner = empfangeneNachricht.Sender;
+            }
 
-            }        
+            //Existiert Node schon?
+            XmlNodeList Chatkontakte = Sicherungsdatei.SelectNodes("//mainuser/chats/Chatkontakt"); //Alle ChatkontaktNodes laden
 
+            bool gefunden = false;
+            foreach (XmlNode Chatkontakt in Chatkontakte)   //Liste durchgehen, ob Chatkontakt schon gespeichert ist
+            {
+                if (Chatkontakt.Attributes["username"].Value == Chatpartner.Username)
+                {
+                    gefunden = true;
+
+                    //NachrichtNode erstellen
+                    XmlNode Nachricht = Sicherungsdatei.CreateElement("nachricht");
+
+                    XmlAttribute Sendername = Sicherungsdatei.CreateAttribute("sendername");
+                    XmlAttribute Empfängername = Sicherungsdatei.CreateAttribute("empfängername");
+                    XmlAttribute Zeitpunkt = Sicherungsdatei.CreateAttribute("zeitpunkt");
+                    XmlAttribute Abgeschickt = Sicherungsdatei.CreateAttribute("abgeschickt");
+                    Sendername.Value = empfangeneNachricht.Sender.Username;
+                    Empfängername.Value = empfangeneNachricht.Empfänger.Username;
+                    Zeitpunkt.Value = empfangeneNachricht.Zeitpunkt.ToString();
+                    Abgeschickt.Value = empfangeneNachricht.Abgeschickt.ToString();
+                    Nachricht.InnerText = empfangeneNachricht.Nachrichtentext;
+
+                    Nachricht.Attributes.Append(Sendername);
+                    Nachricht.Attributes.Append(Empfängername);
+                    Nachricht.Attributes.Append(Zeitpunkt);
+                    Nachricht.Attributes.Append(Abgeschickt);
+
+                    Chatkontakt.AppendChild(Nachricht);
+                }
+            }
+
+            if (gefunden == false)  //falls Chatkontakt nicht gefunden wurde
+            {
+                XmlNode Chats = Sicherungsdatei.SelectSingleNode("//mainuser/chats");
+
+                //Erstelle ChatkontaktNode
+                XmlNode Chatkontakt = Sicherungsdatei.CreateElement("chatkontakt");
+                XmlAttribute Username = Sicherungsdatei.CreateAttribute("username");
+                Chatkontakt.Attributes.Append(Username);
+
+                //Erstelle NachrichtNode
+                XmlNode Nachricht = Sicherungsdatei.CreateElement("nachricht");
+
+                XmlAttribute Sendername = Sicherungsdatei.CreateAttribute("sendername");
+                XmlAttribute Empfängername = Sicherungsdatei.CreateAttribute("empfängername");
+                XmlAttribute Zeitpunkt = Sicherungsdatei.CreateAttribute("zeitpunkt");
+                XmlAttribute Abgeschickt = Sicherungsdatei.CreateAttribute("abgeschickt");
+                Sendername.Value = empfangeneNachricht.Sender.Username;
+                Empfängername.Value = empfangeneNachricht.Empfänger.Username;
+                Zeitpunkt.Value = empfangeneNachricht.Zeitpunkt.ToString();
+                Abgeschickt.Value = empfangeneNachricht.Abgeschickt.ToString();
+                Nachricht.InnerText = empfangeneNachricht.Nachrichtentext;
+
+                Nachricht.Attributes.Append(Sendername);
+                Nachricht.Attributes.Append(Empfängername);
+                Nachricht.Attributes.Append(Zeitpunkt);
+                Nachricht.Attributes.Append(Abgeschickt);
+
+                Chatkontakt.AppendChild(Nachricht);
+                Chatkontakt.AppendChild(Chatkontakt);
+            }
+
+            //Datei wieder speichern
+            Sicherungsdatei.Save(pfad);
         }
 
+        public static void NeueSicherungsdateiErstellen(User Mainuser)
+        {
+            //Erstellt eine völlig neue Sicherungsdatei, falls das Programm zum ersten mal gestartet wird
+            XmlDocument Sicherungsdatei = new XmlDocument();
+
+            XmlNode Mainusernode = Create_Usernode("mainuser", Mainuser.Username);
+            Sicherungsdatei.AppendChild(Mainusernode);
+
+            XmlNode Chats = Sicherungsdatei.CreateElement("chats");
+            Sicherungsdatei.AppendChild(Chats);
+
+            XmlNode Create_Usernode(string nodename, string username)   //evtl wieder löschen?!
+            {
+                XmlNode user_Node = Sicherungsdatei.CreateElement(nodename);
+                XmlAttribute Username = Sicherungsdatei.CreateAttribute("username");
+                Username.Value = username;
+                return user_Node;
+            }
+
+            Sicherungsdatei.Save(pfad); //Datei speichern
+        }
     }
 
     class Laden
     {
+        //Pfad unter dem die Sicherungsdatei abgelegt ist
         const string pfad = @"Sicherungsdatei.xml";
 
         public static List <Nachricht> Nachrichten_Laden(User aktueller_Chatpartner)
@@ -64,16 +133,31 @@ namespace Chatprogramm_github
         }
         
         public static User Username_Laden()
-        {
-            //Fehler werfen wenn dokument nicht vorhanden                       
+        {                     
+            User Mainuser = new User();
             if (File.Exists(pfad))
             {
                 XmlDocument Sicherungsdatei = new XmlDocument();
                 Sicherungsdatei.Load(pfad);
+                XmlNode MainUserNode = Sicherungsdatei.SelectSingleNode("//mainuser");
+                Mainuser.Username = MainUserNode.Attributes["username"].Value;
             }
-            
-            return new User();
+            else
+            {
+                //Ein Dialog zur Eingabe des Usernamens wird erstellt und angezeigt            
+                
+                Username_Dialog dlg = new Username_Dialog();
+                dlg.ShowDialog();
+                if (dlg.DialogResult == true)
+                {
+                    Mainuser = dlg.ReturnUser();
+                }
+                //War die Erstellung des Users erfolgreich, wird eine Messagebox angezeigt
+                //MessageBox.Show("Sie haben sich als \"" + Mainuser.Username + "\" angemeldet.", "Anmeldung erfolgreich");
+                //Usernameeingabe war erfolgreich und wird ausgegeben.
+                Speicherung.NeueSicherungsdateiErstellen(Mainuser);
+            }
+            return Mainuser;
         }
-    //FUnktion Überprüfen ob dok gibt und Usernamen auslesen! Nur Hier prüfen und in Speicherung dann nur Load des Doks. Dann Username zurückgeben
     }
 }
