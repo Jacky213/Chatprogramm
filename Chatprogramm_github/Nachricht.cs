58af2525b634bca3b5c2c2b60cfacaf5865f6970
Jacky213 <jkaefer@bluewin.ch>
2019-01-24 13:08:36 +0100

    Kommentare hinzugefügt in Nachrichten und Speicherung

--- Chatprogramm_github/Nachricht.cs
+++ Chatprogramm_github/Nachricht.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*Haeder
+ * 24.01.2019, Jacqueline Kaefer und Luca Katzenberger
+ *Diese Klasse definiert das Objekt Nachricht mit all seinen Eigenschaften.
+ * Objekte dieser Klassen enthalten unterschiedliche Eigenschaften, um wichtige Informationen zu einer Nachricht zusammenzuführen.
+ * Die Klasse enthält ausserdem zwei Konstruktoren und mehrere Methoden, unter anderem zum Codieren und Decodieren von Nachrichten.
+ */
+
+
+using System;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -7,14 +15,16 @@ namespace Chatprogramm_github
 {
     public class Nachricht
     {
-        //Eigenschaften
+        #region Attributes
         public string Nachrichtentext;
         public User Sender;   //20 Zeichen
         public User Empfänger;    //20 Zeichen
         public DateTime Zeitpunkt;  //19 Zeichen
         public bool Abgeschickt;    //4 Zeichen
+        #endregion
 
-        //Konstruktoren
+
+        #region Constructors
         public Nachricht(string nachrichtentext, User sender, User empfänger, DateTime zeitpunkt, bool abgeschickt)
         {
             this.Nachrichtentext = nachrichtentext;
@@ -29,38 +39,49 @@ namespace Chatprogramm_github
             this.Sender = new User();
             this.Empfänger = new User();
         }
+        #endregion
 
-        //Methoden
-        //bytearray in Methoden einfügen    !erledigt!
+        #region Methods
         public byte[] NachrichtCodieren()
         {
+            //Diese Methode erzeugt aus einem Objekt Nachricht einen codierten String, der dann verschickt werden kann. Dieser String wird dann als Byte-Array zurückgegeben.
+
             //Protokoll: Sender,Empfänger,Zeitpunkt,Abgeschickt,Nachricht 
+
             string Codierung = "";
             string username = Sender.Username; 
             while (username.Length < 20)
             {
-                username += " ";
+                username += " "; //Der Usernamen wird mit Leerzeichen auf 20 Zeichen verlängert.
             }
-            Codierung += username  + "$%&";
+            Codierung += username  + "$%&"; //An den Usernamen wird das Trennzeichen zum Trennen der Informationen angehängt.
             string empfängername = Empfänger.Username;
             while (empfängername.Length < 20)
             {
-                empfängername += " ";
+                empfängername += " "; //Auch der Empfängername wird auf 20 Zeichen verlängert.
             }
+            //Die anderen Informationen werden angehängt
             Codierung += empfängername + "$%&";
             Codierung += Zeitpunkt.ToString() + "$%&";
             Codierung += Abgeschickt.ToString() + "$%&";
-            Codierung += Nachrichtentext;
+            Codierung += Nachrichtentext; 
 
+            //Der String wird zu einem Byte-Array konvertiert
             byte[] data = Encoding.Unicode.GetBytes(Codierung);
-            return data;
+
+            return data; //Der Byte-Array wird zurückgegeben
         }
+        
 
         public static Nachricht NachrichtDecodieren (string Codierung)
         {
+            //Diese Nachricht erhält einen Byte-Array, der Empfangen worden ist und erstellt daraus ein Objekt Nachricht, das zurückgegeben wird. 
             Nachricht Empfang = new Nachricht();
             string[]  EmpfangeneDaten = new string[5];
             
+            // Der Byte-Array wird in einen String konvertiert. Dieser String wird an den Trennzeichen aufgetrennt und die Substrings in einem String-Array gespeichert.
+            //Die einzelnen Substrings werden dann in den Eigenschaften der Nachricht geschrieben.
+
             EmpfangeneDaten = Codierung.Split(new string[] { "$%&"},StringSplitOptions.None);
             Empfang.Sender.Username = EmpfangeneDaten[0].Trim();
             Empfang.Empfänger.Username = EmpfangeneDaten[1].Trim();
@@ -73,6 +94,8 @@ namespace Chatprogramm_github
 
         public TextBox NachrichtinTextbox()
         {
+            //Diese Funktion erstellt eine Textbox, die formatiert wird und den Nachrichtentext enthält. Diese Textbox wird zurückgegeben.
+
             TextBox mytxt = new TextBox();
             mytxt.Text = this.Nachrichtentext;
             mytxt.Width = 500;
@@ -81,5 +104,6 @@ namespace Chatprogramm_github
 
             return mytxt;
         }
+        #endregion
     }
 }
--- Chatprogramm_github/Speicherung.cs
+++ Chatprogramm_github/Speicherung.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections.Generic;
+﻿/*Haeder
+ * 24.01.2019, Jacqueline Kaefer und Luca Katzenberger
+ * Diese Datei enthält Funktionen, um Daten zu speichern und diese auch wieder zu laden.
+ * Sie ist in mehrere Klasse unterteilt.
+ * Die erste Klasse Speicherung enthält mehrere Funktionen zum Erstellen einer Sicherungsdatei und zum Speichern von Daten darin.
+ * Die zweite Klasse Laden enthält Funktionen zum Laden von unterschiedlichen Daten aus der Speicherungsdatei.
+ */
+
+
+using System.Collections.Generic;
 using System.Xml;
 using System.IO;
 using System.Windows;
@@ -9,11 +18,16 @@ namespace Chatprogramm_github
 
     class Speicherung
     {
-        //Pfad unter dem die SIcherungsdatei gespeichert wird
+        #region Constants
+        //Pfad unter dem die Sicherungsdatei gespeichert wird
         const string pfad = @"Sicherungsdatei.xml";
+        #endregion
 
+        #region Methods
         public static void Speichern(User Mainuser, Nachricht empfangeneNachricht)
         {
+            //Diese Methode speichert die mitgegebenene Nachricht in der Sicherungsdatei ab. 
+            
             //Sicherungsdatei laden
             XmlDocument Sicherungsdatei = new XmlDocument();
             Sicherungsdatei.Load(pfad);
@@ -35,28 +49,32 @@ namespace Chatprogramm_github
             bool gefunden = false;
             foreach (XmlNode Chatkontakt in Chatkontakte)   //Liste durchgehen, ob Chatkontakt schon gespeichert ist
             {
-                if (Chatkontakt.Attributes["username"].Value == Chatpartner.Username)
+                if (Chatkontakt.Attributes["username"].Value == Chatpartner.Username) //True, wenn der Kontakt schon in der Sicherungsdatei enthalten ist.
                 {
                     gefunden = true;
 
-                    //NachrichtNode erstellen
+                    //Neue NachrichtNode erstellen mit all ihren Attributen
                     XmlNode Nachricht = Sicherungsdatei.CreateElement("nachricht");
 
                     XmlAttribute Sendername = Sicherungsdatei.CreateAttribute("sendername");
                     XmlAttribute Empfängername = Sicherungsdatei.CreateAttribute("empfängername");
                     XmlAttribute Zeitpunkt = Sicherungsdatei.CreateAttribute("zeitpunkt");
                     XmlAttribute Abgeschickt = Sicherungsdatei.CreateAttribute("abgeschickt");
+
+                    //Den Attributen werden Values zugeordnet.
                     Sendername.Value = empfangeneNachricht.Sender.Username;
                     Empfängername.Value = empfangeneNachricht.Empfänger.Username;
                     Zeitpunkt.Value = empfangeneNachricht.Zeitpunkt.ToFileTime().ToString();
                     Abgeschickt.Value = empfangeneNachricht.Abgeschickt.ToString();//.ToString();
                     Nachricht.InnerText = empfangeneNachricht.Nachrichtentext;
 
+                    //Die Attribute werden an die Node gehängt
                     Nachricht.Attributes.Append(Sendername);
                     Nachricht.Attributes.Append(Empfängername);
                     Nachricht.Attributes.Append(Zeitpunkt);
                     Nachricht.Attributes.Append(Abgeschickt);
 
+                    //Und die Node an den Chatkontakt
                     Chatkontakt.AppendChild(Nachricht);
                 }
             }
@@ -65,13 +83,13 @@ namespace Chatprogramm_github
             {
                 XmlNode Chats = Sicherungsdatei.SelectSingleNode("//mainuser/chats");
 
-                //Erstelle ChatkontaktNode
+                //Erstellen einer neuen ChatkontaktNode mit ihrem Attribut
                 XmlNode Chatkontakt = Sicherungsdatei.CreateElement("chatkontakt");
                 XmlAttribute Username = Sicherungsdatei.CreateAttribute("username");
                 Username.Value = Chatpartner.Username;
                 Chatkontakt.Attributes.Append(Username);
 
-                //Erstelle NachrichtNode
+                //Erstelle neue NachrichtNode
                 XmlNode Nachricht = Sicherungsdatei.CreateElement("nachricht");
 
                 XmlAttribute Sendername = Sicherungsdatei.CreateAttribute("sendername");
@@ -89,6 +107,7 @@ namespace Chatprogramm_github
                 Nachricht.Attributes.Append(Zeitpunkt);
                 Nachricht.Attributes.Append(Abgeschickt);
 
+                //Die neuen Nodes werden an die Sicherungsdatei gehängt.
                 Chats.AppendChild(Chatkontakt);
                 Chatkontakt.AppendChild(Nachricht);                
             }
@@ -99,11 +118,10 @@ namespace Chatprogramm_github
 
         public static void NeueSicherungsdateiErstellen(User Mainuser)
         {
-            //Erstellt eine völlig neue Sicherungsdatei, falls das Programm zum ersten mal gestartet wird
+            //Diese Methode erstellt eine völlig neue Sicherungsdatei
             XmlDocument Sicherungsdatei = new XmlDocument();
 
-            XmlNode Mainusernode = Sicherungsdatei.CreateElement("mainuser"); //Create_Usernode("mainuser", Mainuser.Username);
-            //XmlNode user_Node = Sicherungsdatei.CreateElement("mainuser");
+            XmlNode Mainusernode = Sicherungsdatei.CreateElement("mainuser");            
             XmlAttribute Username = Sicherungsdatei.CreateAttribute("username");
             Username.Value = Mainuser.Username;
             Mainusernode.Attributes.Append(Username);
@@ -118,9 +136,11 @@ namespace Chatprogramm_github
 
         public static void Kontakt_löschen(User kontakt)
         {
+            //Diese Methode löscht den mitgegebenen Kontakt und alle zugehörigen Nachrichten aus der Sicherungsdatei
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load(pfad);
 
+            //Es werden neue Nodes erstellt und an die Sicherungsdatei angehängt.
             XmlNode chats = xmlDoc.SelectSingleNode("//mainuser/chats");
             XmlNodeList chatkontakte = xmlDoc.SelectNodes("//mainuser/chats/chatkontakt");
 
@@ -133,35 +153,42 @@ namespace Chatprogramm_github
                 }
                 
             }
+            //Nach der Bearbeitung wird die Datei wieder gespeichert.
             xmlDoc.Save(pfad);
         }
+        #endregion
     }
 
     class Laden
     {
+        #region Constants
         //Pfad unter dem die Sicherungsdatei abgelegt ist
         const string pfad = @"Sicherungsdatei.xml";
+        #endregion
 
+        #region Methods
         public static List <Nachricht> Nachrichten_Laden(User aktueller_Chatpartner)
         {
-            //Dokument laden und aus den Nachrichten mit dem entsprechenden Chatpartner eine Liste mit Nachrichten erstellen und ausgeben
+            //Diese Methode lädt die Sicherungsdatei und gibt alle Nachrichten in Zusamenhang mit dem mitgegebenen User in einer Liste zurück
+           
             XmlNode aktueller_Chatkontakt;
             XmlDocument Sicherungsdatei = new XmlDocument();
             Sicherungsdatei.Load(pfad);
 
             List<Nachricht> Nachrichten = new List<Nachricht>();
-            Nachrichten.Clear();
+            Nachrichten.Clear(); //Damit die Liste auf jeden Fall leer ist.
 
             XmlNodeList Chatkontakte = Sicherungsdatei.SelectNodes("//mainuser/chats/chatkontakt"); //Alle ChatkontaktNodes laden
 
             foreach(XmlNode Chatkontakt in Chatkontakte)
             {
-                if(Chatkontakt.Attributes["username"].Value == aktueller_Chatpartner.Username)
+                if(Chatkontakt.Attributes["username"].Value == aktueller_Chatpartner.Username) //Suchen der Node, die zum mitgegebenen User gehört
                 {
                     aktueller_Chatkontakt = Chatkontakt;
                     XmlNodeList nachrichten = aktueller_Chatkontakt.ChildNodes;
                     foreach (XmlNode nachricht in aktueller_Chatkontakt)
                     {
+                        //Aus dem Inhalt der Sicherungsdatei wird ein Objekt Nachricht erzeugt
                         string nachrichtentext = nachricht.InnerText;
                         User Sender = new User(nachricht.Attributes["sendername"].Value);
                         User Empfänger = new User(nachricht.Attributes["empfängername"].Value);
@@ -169,18 +196,25 @@ namespace Chatprogramm_github
                         bool abgeschickt = System.Convert.ToBoolean(nachricht.Attributes["abgeschickt"].Value);
 
                         Nachricht hilfe = new Nachricht(nachrichtentext, Sender,Empfänger, dateTime,abgeschickt);
+
+                        //Die Nachricht wird der List hinzugefügt.
                         Nachrichten.Add(hilfe);
                     }
                 }
             }
-            return Nachrichten;
+            return Nachrichten; //Die Liste mit Nachrichten wird zurückgegeben
         }
         
-        public static User Username_Laden() //Hier auch Kontakte laden
-        {                     
+        public static User Username_Laden()
+        {        
+            //Diese Funktion prüft, ob bereits eine Sicherungsdatei existiert. Wenn ja, gibt sie den darin gespeicherten Mainuser zurück. Wenn nicht ruft sie eine Methode auf,
+            //die eine neue Sicherungsdatei erstellt, nachdem sie den Username_Dialog zum Festlegen eines Mainusers aufgerufen hat.
+
             User Mainuser = new User();
-            if (File.Exists(pfad))
+
+            if (File.Exists(pfad))//Existiert schon eine Sicherungsdatei?
             {
+                //Wenn ja, wird der Mainuser geladen
                 XmlDocument Sicherungsdatei = new XmlDocument();
                 Sicherungsdatei.Load(pfad);
                 XmlNode MainUserNode = Sicherungsdatei.SelectSingleNode("//mainuser");
@@ -189,22 +223,22 @@ namespace Chatprogramm_github
             else
             {
                 //Ein Dialog zur Eingabe des Usernamens wird erstellt und angezeigt
+
                 Username_Dialog dlg = new Username_Dialog();
                 dlg.ShowDialog();
                 if (dlg.DialogResult == true)
                 {
                     Mainuser = dlg.ReturnUser();
                 }
-                //War die Erstellung des Users erfolgreich, wird eine Messagebox angezeigt
-                ////MessageBox.Show("Sie haben sich als \"" + Mainuser.Username + "\" angemeldet.", "Anmeldung erfolgreich");
-                //Usernameeingabe war erfolgreich und wird ausgegeben.
+                //Danach wird eine neue Sicherungsdatei erstellt.
                 Speicherung.NeueSicherungsdateiErstellen(Mainuser);
             }
-            return Mainuser;
+            return Mainuser; //Der Mainuser wird zurückgegeben
         }
 
         public static List<User> Kontakte_laden()
         {
+            //Diese Methode lädt alle Kotakte aus der Sicherungsdatei in eine Liste mit Usern und gibt diese zurück. Existiert keine Sicherungsdatei gibt diese Methode eine leere Liste zurück.
             List<User> Kontaktliste = new List<User>();
 
             if (File.Exists(pfad))
@@ -220,5 +254,6 @@ namespace Chatprogramm_github
             }
             return Kontaktliste;
         }
+        #endregion
     }
 }
